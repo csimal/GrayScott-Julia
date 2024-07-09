605de4b46cb4bb6b8c07169adfabba9511e1bcb5
csimal <cedric.simal@unamur.be>
2024-07-09 16:26:18 +0200

    Example script (gross)

--- examples/simple.jl
+++ examples/simple.jl
@@ -1,23 +1,53 @@
-include("../src/GrayScott.jl")
+using Revise
 
-using .GrayScott
-using .GrayScott: SimpleGrayScott
-using .GrayScott: update!, output!
+using Distributed
+addprocs(8)
+
+@everywhere include("../src/GrayScott.jl")
+push!(LOAD_PATH, pwd() * "/src")
+
+
+@everywhere using .GrayScott
+
+using .GrayScott: SimpleGrayScott, AdvancedGrayScott, TurboGrayScott
+using .GrayScott: ThreadedGrayScott, ParallelGrayScott
+using .GrayScott: update!, output!, laplacian!
 using BenchmarkTools
+using GLMakie
 
-opts = GrayScottOptions(nrow=100, ncol=100, num_output_steps=50)
+
+
+opts = GrayScottOptions(nrow=256, ncol=256, num_output_steps=50, Δt=1.0)
 
 params = GrayScottParams()
 
 backend = SimpleGrayScott()
 
+@time simulate(opts, params, SimpleGrayScott())
+
+@time simulate(opts, params, AdvancedGrayScott())
+
+@time simulate(opts, params, TurboGrayScott())
+
+@time simulate(opts, params, ParallelGrayScott())
+
 @time simulate(opts, params, backend)
 
-init_cond = initial_state(opts)
+init_cond = initial_condition(opts)
 out = similar(init_cond)
-x = initial_state(init_cond, backend)
-dx = similar(x)
+x, dx = initial_state(init_cond, backend)
+
+heatmap(init_cond[1,:,:])
 
 @benchmark update!(dx, x, params, backend)
 
 @benchmark output!(out, x, backend)
+
+
+@benchmark update!(dx, x, params, AdvancedGrayScott())
+
+@benchmark update!(dx, x, params, TurboGrayScott())
+
+
+
+@benchmark laplacian!()
